993820b8038a045ab0a422ac19c3d1e3ac20605b
Angie Cohen <angiedev2001@gmail.com>
2023-05-09 14:24:07 -0300

    Agregamos TypeOrm y Mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,23 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { VideosModule } from './videos/videos.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [AuthModule, UsersModule, VideosModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    VideosModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql', // aquí debe especificar su tipo de base de datos, puede ser mysql, postgresql, sqlite, etc.
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'password',
+      database: 'testdb',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
