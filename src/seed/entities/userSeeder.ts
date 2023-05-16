011859187ab2cdf05ee566681b508e8bba33f100
enoc soto <tecenocdejesus@gmail.com>
2023-05-16 12:18:56 -0500

    se crea seed con 19 inserciones de usuarios a la base de datos utilizando promisse.all

--- src/seed/entities/userSeeder.ts
+++ src/seed/entities/userSeeder.ts
@@ -3,24 +3,36 @@ import { DataSource } from 'typeorm';
 import { Seeder, SeederFactoryManager } from 'typeorm-extension';
 import * as bcrypt from 'bcrypt';
 import { randomUUID } from 'crypto';
+import { faker } from '@faker-js/faker';
 
-export class UserSeeder implements Seeder{
-   async run(dataSource: DataSource, factoryManager: SeederFactoryManager): Promise<any> {
-      const userRepository = dataSource.getRepository(User)
+export class UserSeeder implements Seeder {
+  async run(dataSource: DataSource, factoryManager: SeederFactoryManager): Promise<any> {
+    const userRepository = dataSource.getRepository(User)
 
-      const userData= {
+    const userData = await this.createFake()
+    const dataCreated = await Promise.all(userData.map(item => userRepository.create(item)))
+    const dataFull = await Promise.all(dataCreated.map(item => userRepository.save(item)))
+
+    return dataFull
+  }
+
+  async createFake() {
+    const data: Array<User> = []
+    for (let i = 0; i < 20; i++) {
+      data.push({
         id: randomUUID(),
-        name    : 'example',
-        lastname: 'second example',
-        username: 'userexample',
+        name: faker.person.firstName('male'),
+        lastname: faker.person.lastName('male'),
+        username: faker.internet.userName(),
         password: await bcrypt.hash('example', 8),
-        email   : 'examplemail@mail.com',
-      }
-      const newUser = userRepository.create(userData);
-      await userRepository.save(newUser);
+        email: faker.internet.email(),
+        createdat: faker.date.anytime(),
+        updatedat: faker.date.anytime(),
+        videos: null
+      })
     }
+
+    return data
+  }
 }
 
-function UUID() {
-    throw new Error('Function not implemented.');
-}
